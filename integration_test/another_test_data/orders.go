40d78b4dab33f618cb969882cf09a2b7568b383a
nakatsu-kouki <134385872+nakatsu-kouki@users.noreply.github.com>
2024-06-10 12:41:18 +0000

    fix table

--- integration_test/another_test_data/orders.go
+++ integration_test/another_test_data/orders.go
@@ -3,9 +3,9 @@ package anothertestdata
 import testdata "github.com/digeon-inc/royle/integration_test/test_data"
 
 type Orders struct {
-	ID          int32          `gorm:"primaryKey;autoIncrement;comment:'Primary Key';"`
-	ProductName string         `gorm:"size:255;not null;comment:'Name of the product';"`
-	UserID      int32          `gorm:"index;comment:'Foreign key to the user';"`
-	Quantity    int32          `gorm:"default:1;comment:'Quantity of the product being ordered, defaults to 1';"`
+	ID          int32          `gorm:"primaryKey;autoIncrement;"`
+	ProductName string         `gorm:"size:255;not null;"`
+	UserID      int32          `gorm:"index;"`
+	Quantity    int32          `gorm:"default:1;comment:Quantity of the product being ordered, defaults to 1;"`
 	User        testdata.Users `gorm:"foreignKey:UserID"`
 }
--- integration_test/test_data/users.go
+++ integration_test/test_data/users.go
@@ -1,7 +1,7 @@
 package testdata
 
 type Users struct {
-	ID    int32  `gorm:"primaryKey;autoIncrement;comment:'Primary Key';"`
-	Name  string `gorm:"size:255;not null;comment:'Name of the user';"`
-	Email string `gorm:"size:255;not null;unique;comment:'Unique email address';"`
+	ID    int32  `gorm:"primaryKey;autoIncrement;"`
+	Name  string `gorm:"size:255;not null;"`
+	Email string `gorm:"size:255;not null;unique;"`
 }
