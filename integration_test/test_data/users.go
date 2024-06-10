f55116583fd839933470d533690fcc9c6d2ea4eb
nakatsu-kouki <134385872+nakatsu-kouki@users.noreply.github.com>
2024-06-10 12:36:30 +0000

    fix table

--- integration_test/test_data/users.go
+++ integration_test/test_data/users.go
@@ -1,7 +1,7 @@
 package testdata
 
 type Users struct {
-	ID    uint   `gorm:"primaryKey;autoIncrement;comment:'Primary Key'"`
-	Name  string `gorm:"size:255;not null;comment:'Name of the user'"`
-	Email string `gorm:"size:255;not null;unique;comment:'Unique email address'"`
+	ID    int32  `gorm:"primaryKey;autoIncrement;comment:'Primary Key';"`
+	Name  string `gorm:"size:255;not null;comment:'Name of the user';"`
+	Email string `gorm:"size:255;not null;unique;comment:'Unique email address';"`
 }
